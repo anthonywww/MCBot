c33ecb89b4e5a6e7ad16bfee7abf8071b88c0097
anthonywww <31651690+anthonywww@users.noreply.github.com>
2019-05-21 22:13:52 -0700

    Updated GotoCommand.java

--- src/com/github/anthonywww/mcbot/cli/commands/GotoCommand.java
+++ src/com/github/anthonywww/mcbot/cli/commands/GotoCommand.java
@@ -1,6 +1,6 @@
 package com.github.anthonywww.mcbot.cli.commands;
 
-import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.github.anthonywww.mcbot.MCBot;
 import com.github.anthonywww.mcbot.cli.ICLICommand;
@@ -8,29 +8,37 @@ import com.github.anthonywww.mcbot.utils.Vector3d;
 
 public class GotoCommand implements ICLICommand {
 	
+	private static final Logger logger = Logger.getLogger("");
+	
 	@Override
 	public void invoke(String[] params) {
+		
+		// TODO: add player support
 		
 		if (params.length == 3) {
 			try {
 				final Vector3d position = MCBot.getInstance().getPlayer().getPosition();
-				double dx = Double.parseDouble(params[0]) + 0.5;
-				double dy = Double.parseDouble(params[1]) + 0.5;
-				double dz = Double.parseDouble(params[2]) + 0.5;
-				MCBot.getInstance().log(Level.INFO, "Walking to [" + dx + ", " + dy + ", " + dz + "] ...");
+				double x = Double.parseDouble(params[0]) + 0.5;
+				double y = Double.parseDouble(params[1]);
+				double z = Double.parseDouble(params[2]) + 0.5;
 				
-				dx -= position.getX();
-				dy -= position.getY();
-				dz -= position.getZ();
+				logger.info("Walking to X=" + x + ", Y=" + y + ", Z=" + z + " ...");
+				
+				MCBot.getInstance().getPlayer().face(x, y, z);
+				
+				// Get delta's
+				x -= position.getX();
+				y -= position.getY();
+				z -= position.getZ();
 				
 				// TODO: Use path finding
-				MCBot.getInstance().getPlayer().move(dx, dy, dz);
+				MCBot.getInstance().getPlayer().move(x, y, z);
 				
 				return;
 			} catch (NumberFormatException e) {}
 		}
 		
-		MCBot.getInstance().log(Level.INFO, "Usage: " + commandName() + " <x> <y> <z>");
+		logger.info("Usage: " + commandName() + " <x> <y> <z>");
 	}
 	
 	@Override
